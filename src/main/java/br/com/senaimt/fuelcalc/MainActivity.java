60fec4bea5fe864d2eb2c334ba3689d7016b9c66
Lucas Diego <luucasdiego_12@hotmail.com>
2021-11-05 23:27:45 -0400

    Função calcular e visualizar resultado

--- src/main/java/br/com/senaimt/fuelcalc/MainActivity.java
+++ src/main/java/br/com/senaimt/fuelcalc/MainActivity.java
@@ -3,12 +3,47 @@ package br.com.senaimt.fuelcalc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Componentes de tela
+    TextView txtValorAlcool;
+    TextView txtValorGasolina;
+    Button btnVerificar;
+    TextView txtResultado;
+    String mensagem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtValorAlcool = findViewById(R.id.txtValorAlcool);
+        txtValorGasolina = findViewById(R.id.txtValorGasolina);
+        btnVerificar = findViewById(R.id.btnVerificar);
+        txtResultado = findViewById(R.id.txtResultado);
+
+        try {
+            String valorAlcool = txtValorAlcool.getText().toString();
+            String valorGasolina = txtValorGasolina.getText().toString();
+            float convertAlcool = Float.parseFloat(valorAlcool);
+            float convertGasolina = Float.parseFloat(valorGasolina);
+
+            float resultado = convertAlcool / convertGasolina;
+            if (resultado < 0.7) {
+                mensagem = "É melhor abastecer com Álcool!";
+            } else {
+                mensagem = "É melhor abastecer com Gasolina!";
+            }
+            txtResultado.setText(mensagem);
+            txtResultado.setVisibility(View.VISIBLE);
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "Valor incorreto!", Toast.LENGTH_LONG);
+        }
+
     }
 }
